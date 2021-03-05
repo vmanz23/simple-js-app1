6bdf5fcdeef26bdbdeeaa03aa1a6f5da979b4226
vmanz23 <vintagemodern@zoho.com>
2021-03-05 13:15:10 -0800

    pokedex variable array creation
    4 pokemon added to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Gryados', height: 6.5, types: ['water', 'flying'] },
+    { name: 'Mewtwo', height: 2, types: ['psychic'] },
+    { name: 'Lapras', height: 2.5, types: ['ice', 'water']},
+    { name: 'Scyther',  height: 1.5, types: ['bug', 'flyting'] }];
