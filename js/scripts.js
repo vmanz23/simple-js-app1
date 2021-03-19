a3f4724d96bbe8da5e18bd7a2baa1a6f63191353
vmanz23 <vintagemodern@zoho.com>
2021-03-19 10:49:09 -0700

    foreach outside of IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,33 @@
 let pokemonRepository = (function () {
+  function getAll(){
+    return pokemonList;
+  }
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+return {
+  getAll: getAll,
+  add:add
+};
+})();
+
   let pokemonList = [  {name: "Gryados", height: 6.5, type: "flying,water"},
   {name: "Mewtwo", height: 2, type: "psychic"},
   {name: "Scyther", height:1.5, type:"bug, flying"}]; 
 
   pokemonList.forEach(function(pokemon) {
-    let pokemonName = pokemon.name
-    let pokemonHeight = pokemon.height
-    if (pokemonHeight >5) {
-      document.write (pokemonName + " :  " + pokemonHeight + '   wow thats big!  ');
-    } else{
-      document.write(pokemonName + "  :  " + pokemonHeight);
-    }
-    document.write('<br>');
-    });
-  
-    
-  return {
-  add: function(pokemon) {
-  pokemonList.push(pokemon);
-  },
-  getAll: function() {
-  return pokemonList;
+  let pokemonName = pokemon.name
+  let pokemonHeight = pokemon.height
+  if (pokemonHeight >5) {
+    document.write (pokemonName +'  :  '+ pokemonHeight +'  wow, thats big!  ');
+  } else{
+    document.write(pokemonName + '  :  '+ pokemonHeight);
   }
-  };
-  })();
+  document.write('<br>');
+  });
+ 
+  console.log(pokemonRepository.getAll() );
+
+
+  
+  
