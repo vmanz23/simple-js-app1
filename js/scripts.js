f1b77619eb633e2692d8177a6273986d9eb21094
vmanz23 <vintagemodern@zoho.com>
2021-03-19 09:34:49 -0700

    array inside IIFE , foreach

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,26 @@
 let pokemonRepository = (function () {
+  let pokemonList = [  {name: "Gryados", height: 6.5, type: "flying,water"},
+  {name: "Mewtwo", height: 2, type: "psychic"},
+  {name: "Scyther", height:1.5, type:"bug, flying"}]; 
+
+  pokemonList.forEach(function(pokemon) {
+    let pokemonName = pokemon.name
+    let pokemonHeight = pokemon.height
+    if (pokemonHeight >5) {
+      document.write (pokemonName + " :  " + pokemonHeight + '   wow thats big!  ');
+    } else{
+      document.write(pokemonName + "  :  " + pokemonHeight);
+    }
+    document.write('<br>');
+    });
   
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-  function getAll() {
-    return pokemonList;
-  }
-
+    
   return {
-    add: add,
-    getAll: getAll
-  };
-  
-})();
-
-let pokemonList = [{ name: 'Gyrados', height: '6.5', types: ['flying', 'water']}, 
-{ name: 'Mewtwo', height: '2', types: ['psychic']}, 
-{ name: 'Scyther', height: '1.5', types: ['bug', 'flying']}, ];
-
-pokemonList.forEach(function(pokemon) {
-  let pokemonName = pokemon.name
-  let pokemonHeight = pokemon.height
-  if (pokemonHeight >5) {
-    document.write (pokemonName + '  :  '+ pokemonHeight + '  Wow, thats big!  ');
-  } else{
-    document.write(pokemonName + '  :  '+ pokemonHeight);
+  add: function(pokemon) {
+  pokemonList.push(pokemon);
+  },
+  getAll: function() {
+  return pokemonList;
   }
-  document.write('<br>'), ('<br>');
-  });
-
-  console.log (pokemonList);
+  };
+  })();
